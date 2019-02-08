f7f26682a713b06a2c57cc1eebe62c1878e58544
claudiopo <claudiopoinostropo@gmail.com>
2019-02-08 13:04:08 -0300

    Parpadeo animacion de personajes con textura

--- Assets/AnimatorBoolController.cs
+++ Assets/AnimatorBoolController.cs
@@ -4,39 +4,36 @@ using UnityEngine;
 
 public class AnimatorBoolController : MonoBehaviour
 {
-    //Claudio Inostroza
+    //Claudio Inostroza shit script
 
     public Animator animator;//Animator to control
     public bool[] behavior;//List Of bool
     public string[] BehaviorName;//list of Name of the bool in the animator controller
-
+   
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-
+        UpdateBehavior();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateBehavior();
+    }
 
-
-        //animator.SetBool(BehaviorName[0], behavior[0]);
-        //animator.SetBool(BehaviorName[1], behavior[1]);
-        //animator.SetBool(BehaviorName[2], behavior[2]);
-        //animator.SetBool(BehaviorName[3], behavior[3]);
-        //animator.SetBool(BehaviorName[4], behavior[4]);
-
-        if (behavior[0] == true )
+    void UpdateBehavior()
+    {
+        if (behavior[0] == true)
         {
             animator.SetTrigger(BehaviorName[0]);
-            behavior[0]= false;
+            behavior[0] = false;
         }
 
-        if (behavior[1] == true )
+        if (behavior[1] == true)
         {
             animator.SetTrigger(BehaviorName[1]);
             behavior[1] = false;
@@ -65,11 +62,6 @@ public class AnimatorBoolController : MonoBehaviour
             animator.SetTrigger(BehaviorName[5]);
             behavior[5] = false;
         }
-
-        //if (behavior[6] == true)
-        //{
-        //    animator.SetTrigger(BehaviorName[6]);
-        //    behavior[6] = false;
-        //}
     }
+
 }
