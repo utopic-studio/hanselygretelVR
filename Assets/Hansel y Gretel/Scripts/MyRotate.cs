d319048ff7508d8b0bea5d50796151498a680f05
claudiopo <claudiopoinostropo@gmail.com>
2019-02-18 18:03:23 -0300

    Se mato a la bruja (animacion de su muerte , no literalmente)

--- Assets/Hansel y Gretel/Scripts/MyRotate.cs
+++ Assets/Hansel y Gretel/Scripts/MyRotate.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MyRotate : MonoBehaviour {
 
 	public bool global = true;
+
+    public bool OnRotate;
+
 	//[TooltipAttribute("0 for infinite. 1 for one turn. 2 for two... 0.5 for half a turn")]
 	//public float loops = 0f;
 	public Vector3 rotation = Vector3.up;
@@ -11,10 +14,17 @@ public class MyRotate : MonoBehaviour {
 	//private float m_loopCounter;
 
 	void Update () {
-		if (global) {
-			transform.Rotate (rotation, Space.World);
-		} else {
-			transform.Rotate (rotation); //same as transform.Rotate (rotation, Space.Self);
-		}
+
+        if (global)
+        {
+            transform.Rotate(rotation, Space.World);
+        }
+        else
+        {
+            transform.Rotate(rotation); //same as transform.Rotate (rotation, Space.Self);
+        }
+   
 	}
+
+
 }
--- Assets/J/Scripts/Util/JAction.cs
+++ Assets/J/Scripts/Util/JAction.cs
@@ -1,39 +1,55 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 namespace J
 {
 
-	[AddComponentMenu("J/Util/JAction")]
-	public class JAction : MonoBehaviour {
+    [AddComponentMenu("J/Util/JAction")]
+    public class JAction : MonoBehaviour
+    {
 
-		[Range(0.02f, 600f)]
-		[SerializeField]	float delay = 0.2f;
-		[SerializeField]	bool doOnStart = false;
-		[SerializeField]	UnityEngine.Events.UnityEvent normalAction;
-		[SerializeField]	UnityEngine.Events.UnityEvent delayedAction;
+        [Range(0.02f, 600f)]
+       
+        [SerializeField] bool doOnStart = false;
+        [SerializeField] UnityEngine.Events.UnityEvent normalAction;
+        [SerializeField] UnityEngine.Events.UnityEvent[] delayedAction;
+        [SerializeField] float[] delay;
 
-
-		void Start () {
-			if (doOnStart) {
-				CallNormalAction ();
-				CallDelayedAction ();
-			}
-		}
-        public void CallBothActions ()
+        void Start()
+        {
+            if (doOnStart)
+            {
+                CallNormalAction();
+                CallDelayedAction();
+            }
+        }
+        public void CallBothActions()
         {
             CallNormalAction();
             CallDelayedAction();
         }
-		public void CallNormalAction () {
-			normalAction.Invoke ();
+        public void CallNormalAction()
+        {
+            normalAction.Invoke();
 
-		}
-		public void CallDelayedAction () {
-			Invoke ("CallDelayedActionPrivate", delay);
-		}
-		private void CallDelayedActionPrivate() {
-			delayedAction.Invoke ();
-		}
-	}
+        }
+        public void CallDelayedAction()
+        {
+            for (int i = 0; i < delay.Length; i++)
+            {
+                //Invoke("CallDelayedActionPrivate", delay[i]);
+                StartCoroutine("CallDelayedActionPrivate",i);
+                
+                
+            }
+            
+        }
+        IEnumerator CallDelayedActionPrivate(int ActionNumber)
+        {
+            yield return new WaitForSeconds(delay[ActionNumber]);
+            delayedAction[ActionNumber].Invoke();
+  
+        }
+    }
 
 }
