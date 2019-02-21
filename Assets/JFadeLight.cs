673dc114586c1e2779288170733f528ec4344d41
claudiopo <claudiopoinostropo@gmail.com>
2019-02-21 15:52:38 -0300

    Adicion de animaciones al animator controler principal

---
+++ Assets/JFadeLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[AddComponentMenu("J/3D/JFadeLight")]
+public class JFadeLight : MonoBehaviour
+{
+    public bool Fade;
+    public Light FadeLight;
+    public AnimationCurve Intensity;
+    public float Duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeLight.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Fade)
+        {
+            FadeLight.intensity = Intensity.Evaluate(Duration * Time.deltaTime);
+        }
+    }
+}
