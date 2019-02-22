760a606816bab9d9150f71348bb25dd94e27ce6e
claudiopo <claudiopoinostropo@gmail.com>
2019-02-22 18:10:45 -0300

    se recreo lightmap escena 5

--- Assets/JFadeLight.cs
+++ Assets/JFadeLight.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 [AddComponentMenu("J/3D/JFadeLight")]
 public class JFadeLight : MonoBehaviour
 {
@@ -10,11 +11,13 @@ public class JFadeLight : MonoBehaviour
     public Light FadeLight;
     public AnimationCurve Intensity;
     public float Duration;
+    float InitIntensity;
 
     // Start is called before the first frame update
     void Start()
     {
         FadeLight.GetComponent<Light>();
+       
     }
 
     // Update is called once per frame
@@ -22,7 +25,20 @@ public class JFadeLight : MonoBehaviour
     {
         if (Fade)
         {
-            FadeLight.intensity = Intensity.Evaluate(Duration * Time.deltaTime);
+            Duration = Mathf.Clamp(Duration, 0, 200);
+            Duration +=Time.deltaTime;
+            FadeLight.intensity = Intensity.Evaluate(Duration);
+        }
+    }
+    public void OnFade()
+    {
+        if (Fade)
+        {
+            Fade = false;
+        }
+        else
+        {
+            Fade = true;
         }
     }
 }
