a654ca2889b52bfa976cf8e4e7e7fbaa55de7dd3
Pandalex1 <Pandalex1@users.noreply.github.com>
2019-02-25 14:55:02 -0300

    Movimiento del Cisne

---
+++ Assets/Hansel y Gretel/Scripts/TpHanselGretelCisne.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpHanselGretelCisne : MonoBehaviour
+{
+    public GameObject hansel;
+    public GameObject gretel;
+    public GameObject Tp1;
+    public GameObject Tp2;
+    public GameObject cisne;
+    public GameObject movedor;
+    private bool meMuevo = false;
+
+    public void Teleportation() {
+        hansel.transform.position = Tp1.transform.position;
+        hansel.transform.rotation = cisne.transform.rotation;
+        gretel.transform.position = Tp2.transform.position;
+        gretel.transform.rotation = cisne.transform.rotation;
+        StartCoroutine("DelayMovimiento");
+
+    }
+
+    IEnumerator DelayMovimiento() {
+        yield return new WaitForSeconds(2f);
+        movedor.transform.rotation *= Quaternion.Euler(0, 180f, 0);
+        meMuevo = true;
+
+    }
+    private void Update()
+    {
+        if (meMuevo == true)
+        {
+            movedor.transform.Translate(Vector3.back * Time.deltaTime);
+
+        }
+    }
+}
+
