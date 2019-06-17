f28b4a993d7a042e453d5fc2c960d251b83a9026
gabo <gabcorrea@gmail.com>
2019-06-17 18:36:21 -0400

    Todo Ok. Listo para optimización grafica. (v1.3)

--- Assets/J/Scripts/JBeta/JTextLink.cs
+++ Assets/J/Scripts/JBeta/JTextLink.cs
@@ -9,10 +9,23 @@ public class JTextLink : MonoBehaviour
     public string content;
     public UnityEngine.UI.Text text;
     public TMPro.TextMeshProUGUI textPro;
+    [Tooltip("False, se llama una vez en Start(). True, se actualiza en OnValidate()")]
+    public bool update = false;
 
-    void OnValidate()
+    private void Start()
     {
-        
+        JUpdateText();
+    }
+
+    private void OnValidate()
+    {
+        if (update)
+        {
+            JUpdateText();
+        }
+    }
+    public void JUpdateText()
+    {
         if (text)
             text.text = content;
         if (textPro)
