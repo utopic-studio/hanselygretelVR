95948120c938996edd9f7cc89a1adb3e9a105cd9
Carlos Ibanez <carlos.ibanez@criticalfailure-studio.com>
2019-06-18 14:49:16 -0400

    - Added login stage, pending some bug fixes

--- Assets/External/JSONObject/JSONObject.cs
+++ Assets/External/JSONObject/JSONObject.cs
@@ -3,7 +3,7 @@
 #define USEFLOAT	//Use floats for numbers instead of doubles	(enable if you're getting too many significant digits in string output)
 //#define POOLING	//Currently using a build setting for this one (also it's experimental)
 
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 #endif
@@ -11,6 +11,7 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 /*
 Copyright (c) 2015 Matt Schoen
 
@@ -274,7 +275,7 @@ public class JSONObject : IEnumerable {
 			if(strict) {
 				if(str[0] != '[' && str[0] != '{') {
 					type = Type.NULL;
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 					Debug.LogWarning
 #else
 					Debug.WriteLine
@@ -352,9 +353,9 @@ public class JSONObject : IEnumerable {
 						default:
 							try {
 #if USEFLOAT
-								n = System.Convert.ToSingle(str);
+								n = System.Convert.ToSingle(str, CultureInfo.InvariantCulture);
 #else
-								n = System.Convert.ToDouble(str);				 
+								n = System.Convert.ToDouble(str, CultureInfo.InvariantCulture);		 
 #endif
 								if(!str.Contains(".")) {
 									i = System.Convert.ToInt64(str);
@@ -363,7 +364,7 @@ public class JSONObject : IEnumerable {
 								type = Type.NUMBER;
 							} catch(System.FormatException) {
 								type = Type.NULL;
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 								Debug.LogWarning
 #else
 								Debug.WriteLine
@@ -416,7 +417,7 @@ public class JSONObject : IEnumerable {
 								if(type == Type.OBJECT)
 									keys.Add(propName);
 								if(maxDepth != -1)															//maxDepth of -1 is the end of the line
-									list.Add(Create(inner, (maxDepth < -1) ? -2 : maxDepth - 1));
+									list.Add(Create(inner, (maxDepth < -1) ? -2 : maxDepth - 1, storeExcessLevels));
 								else if(storeExcessLevels)
 									list.Add(CreateBakedObject(inner));
 
@@ -445,7 +446,7 @@ public class JSONObject : IEnumerable {
 	public void Add(int val) {
 		Add(Create(val));
 	}
-	public void Add(string str, string palabras) {
+	public void Add(string str) {
 		Add(CreateStringObject(str));
 	}
 	public void Add(AddJSONContents content) {
@@ -694,7 +695,7 @@ public class JSONObject : IEnumerable {
 			}
 		} else if(left.type == Type.ARRAY && right.type == Type.ARRAY) {
 			if(right.Count > left.Count) {
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 				Debug.LogError
 #else
 				Debug.WriteLine
@@ -753,7 +754,7 @@ public class JSONObject : IEnumerable {
 	IEnumerable StringifyAsync(int depth, StringBuilder builder, bool pretty = false) {	//Convert the JSONObject into a string
 		//Profiler.BeginSample("JSONprint");
 		if(depth++ > MAX_DEPTH) {
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 			Debug.Log
 #else
 			Debug.WriteLine
@@ -899,7 +900,7 @@ public class JSONObject : IEnumerable {
 	void Stringify(int depth, StringBuilder builder, bool pretty = false) {	//Convert the JSONObject into a string
 		//Profiler.BeginSample("JSONprint");
 		if(depth++ > MAX_DEPTH) {
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 			Debug.Log
 #else
 			Debug.WriteLine
@@ -1029,7 +1030,7 @@ public class JSONObject : IEnumerable {
 		//Profiler.EndSample();
 	}
 	#endregion
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 	public static implicit operator WWWForm(JSONObject obj) {
 		WWWForm form = new WWWForm();
 		for(int i = 0; i < obj.list.Count; i++) {
@@ -1078,7 +1079,7 @@ public class JSONObject : IEnumerable {
 					case Type.NUMBER: result.Add(keys[i], val.n + ""); break;
 					case Type.BOOL: result.Add(keys[i], val.b + ""); break;
 					default:
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 						Debug.LogWarning
 #else
 						Debug.WriteLine
@@ -1089,7 +1090,7 @@ public class JSONObject : IEnumerable {
 			}
 			return result;
 		}
-#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5 || UNITY_5_3_OR_NEWER
 		Debug.Log
 #else
 		Debug.WriteLine
