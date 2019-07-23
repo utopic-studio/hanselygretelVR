f34406696d84c1b73995674c74b4f13f8c407fca
gabo <gabcorrea@gmail.com>
2019-07-23 12:35:37 -0400

    v3.0 --- Web + Multiples paginas por canvas (usa TextWebX)

--- Assets/J/Scripts/Util/JLoadScene.cs
+++ Assets/J/Scripts/Util/JLoadScene.cs
@@ -9,38 +9,13 @@ namespace J
         
 		[SerializeField]	string sceneName;
 		[SerializeField]	float delay = 0f;
-        [SerializeField]    bool cameraFade = true;
 
-        private JCameraFade2 jCameraFade;
-        private void OnValidate()
-        {
-            _checkFade();
-        }
-        private void Reset()
-        {
-            _checkFade();
-        }
-        private void _checkFade()
-        {
-            // Intentar objener el componente que realiza el fade
-            if (!jCameraFade)
-                jCameraFade = transform.GetComponent<JCameraFade2>();
-            if (!jCameraFade)
-                jCameraFade = GameObject.FindObjectOfType<JCameraFade2>();
-
-        }
         public void JLoadLevel()
         {
             this.LoadTheLevel();
         }
         public void LoadTheLevel () {
-            float _delay = delay;
-            if (jCameraFade)
-            {
-                jCameraFade.JFadeOut();
-                _delay = delay + jCameraFade.fadeoutTime;
-            }
-            Invoke("LoadTheLevelPrivate", _delay);
+            Invoke("LoadTheLevelPrivate", delay);
 		}
         private void LoadTheLevelPrivate () {
 			sceneName = sceneName.Trim ();
